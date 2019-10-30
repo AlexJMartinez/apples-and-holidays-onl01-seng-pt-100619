fd6a5cf2d0e2680d3e9bf2bbee825ebdba24aa84
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-30 22:16:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,11 +67,13 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.map do |season, holidays|
+  
+  new_array = holiday_hash.map do |season, holidays|
   holidays.map do |holiday, supplies|
-    binding.pry
+    
     holiday if supplies.include?("BBQ")
 
    end
- end.flatten.compact
+ end#.flatten.compact
+ binding.pry
 end
